a50079983081f868267e2d91826d1104eb7623c9
YuliKors <82640538+YuliKors@users.noreply.github.com>
2021-04-22 21:29:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        HW2 hw2 = new HW2();
+        hw2.task1();
+        hw2.task2();
+        hw2.task3();
+        hw2.task4();
+        hw2.task5();
+    }
+}
+
